fc157542f5665d9c9d592b708e442c38c109c796
aabhushann <aabhushannikam777@gmail.com>
2023-07-09 22:54:55 +0530

    config updated

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ PORT_NUMBER = 8080
 
 MONGODB_PORT_NUMBER = 27017
 MONGODB_DATABASE_NAME = 'project_db'
-MODEL_FILE_PATH = r'static\pickle_file\linear_regression.pkl'
 
-JSON_FILE_PATH = r'static\json_file\proj_data.json'
+MODEL_FILE_PATH = os.path.join(r'static','pickle_file','linear_regression.pkl')
+
+JSON_FILE_PATH = os.path.join( r'static','json_file','proj_data.json')
